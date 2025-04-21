68acd21f5c0d9ad77e954a11f2c4cafe3f15eb72
Michael Nobles <Mnobles33@gmail.com>
2025-04-21 15:52:39 -0700

    next commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -57,7 +57,13 @@ router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next
   }
  */
 router.post('/login', checkUsernameExists, (req, res, next) => {
-  res.json('login')
+  const { password } = req.body
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user
+    res.json({ message: `Welcome ${req.user.username}!` })
+  } else {
+    next({ status: 401, message: 'Invalid credentials' })
+  }
 })
 
 /**
